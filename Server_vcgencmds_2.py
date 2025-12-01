e49bb6f919901d841f354432a07e0dfe9ff7071a
KyleCode01 <kdkrepelka@gmail.com>
2025-11-30 21:24:25 -0500

    Add files via upload
    updates previous file by adding Clock Frequency using vcgencmd measure_clock arm

---
+++ Server_vcgencmds_2.py
@@ -0,0 +1,64 @@
+# TPRG 2 ASSIGNMENT 2
+# Server_csgencmds.py
+# Kyle Krepelka
+# 100923825
+# Nov 30, 2025
+
+# This server runs on Pi, sends Pi's your 4 arguments from the vcgencmds, sent as Json object.
+
+# details of the Pi's vcgencmds - https://www.tomshardware.com/how-to/raspberry-pi-benchmark-vcgencmd
+# more vcgens on Pi 4, https://forums.raspberrypi.com/viewtopic.php?t=245733
+# more of these at https://www.nicm.dev/vcgencmd/
+
+import socket
+import os
+import time
+import json
+
+s = socket.socket()
+host = '192.168.2.178'
+port = 5000
+s.bind((host, port))
+s.listen(5)
+
+# CPU TEMPERATURE 
+raw_temp = os.popen("vcgencmd measure_temp").readline().strip()
+# Example output: "temp=48.1'C"
+try:
+    temp_value = raw_temp.replace("temp=", "").replace("'C", "")
+    temp_value = float(temp_value)
+except:
+    temp_value = None
+
+
+# CLOCK SPEED
+raw_clock = os.popen("vcgencmd measure_clock arm").readline().strip()
+
+try:
+    clk_value = int(raw_clock.split('=')[1])
+except:
+    clk_value = None
+
+
+
+
+# Build a JSON dictionary
+f_dict = {
+    "CPU_Temperature": {
+        "temp_c": temp_value
+    },
+    "CPU_Clock": {
+        "freq_hz": clk_value,
+        "freq_mhz": clk_value / 1_000_000 if clk_value else None
+        }
+    }
+
+
+while True:
+    c, addr = s.accept()
+    print('Got connection from', addr)
+    
+    json_bytes = json.dumps(f_dict).encode()
+
+    c.send(json_bytes)
+    c.close()
